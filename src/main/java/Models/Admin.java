0b850b07ea7c69f37aae2701d8eb41281932828c
AlbertGirones <73136153+AlbertGirones@users.noreply.github.com>
2023-09-22 08:50:02 +0200

    Add files via upload

---
+++ src/main/java/Models/Admin.java
@@ -0,0 +1,34 @@
+
+package Models;
+
+public class Admin {
+    
+    private int id_admin;
+    private String nick;
+    private String passwd;
+
+    public int getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(int id_admin) {
+        this.id_admin = id_admin;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/proj1padelags/PROJ1PadelAGS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proj1padelags;
+
+/**
+ *
+ * @author geri
+ */
+public class PROJ1PadelAGS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
