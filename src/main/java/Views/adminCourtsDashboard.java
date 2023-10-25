c3172d86dd2bb4022112751cb14df5d46948b4d8
AlbertGirones <agirones600@gmail.com>
2023-10-25 12:33:11 +0200

    Rezizable AdminCourts

--- src/main/java/Views/adminCourtsDashboard.java
+++ src/main/java/Views/adminCourtsDashboard.java
@@ -45,7 +45,7 @@ public class adminCourtsDashboard extends javax.swing.JFrame {
         btnModifyCourtForm = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(575, 445));
+        setResizable(false);
 
         btnInsertCourtForm.setText("Agregar");
         btnInsertCourtForm.addActionListener(new java.awt.event.ActionListener() {
